861ff7999e6fc2465f21e2c604c307c62d6c0acb
Gunj0 <gunjo2012@gmail.com>
2026-01-07 18:48:46 +0900

    ユーザーページ修正

--- src/app/(auth)/user/page.tsx
+++ src/app/(auth)/user/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+
+import { SignIn } from "@/components/auth/sign-in";
 import { SignOut } from "@/components/auth/sign-out";
-import SimpleHeader from "@/components/common/simple-header";
+import LoginHeader from "@/components/common/login-header";
+import { authClient } from "@/lib/auth-client";
 
 export default function UserPage() {
+  const session = authClient.useSession();
+
   return (
     <>
-      <SimpleHeader />
-      <div className="text-center mt-32">
-        <h1 className="mb-10">ユーザーページ</h1>
-        <SignOut />
-      </div>
+      <LoginHeader />
+      {session?.data?.session ? (
+        <div className="text-center min-h-screen mt-10">
+          <p className="mb-10">事前登録ありがとうございます！</p>
+          <p className="mb-10">ログイン中: {session.data.user.email}</p>
+          <SignOut />
+        </div>
+      ) : (
+        <div className="text-center mt-10">
+          <h1 className="mb-10">ログインしてください</h1>
+          <SignIn />
+        </div>
+      )}
     </>
   );
 }
--- src/components/auth/sign-out.tsx
+++ src/components/auth/sign-out.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
